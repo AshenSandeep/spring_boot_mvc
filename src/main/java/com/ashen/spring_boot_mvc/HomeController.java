66ccff04ec70f9e182ec1c584bab536347c354af
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-06-27 08:50:56 +0530

    Created Spring boot application. first commit

---
+++ src/main/java/com/ashen/spring_boot_mvc/HomeController.java
@@ -0,0 +1,28 @@
+package com.ashen.spring_boot_mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return ("index.jsp");
+    }
+
+    @RequestMapping("/add")
+    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
+
+        int num3 = i + j;
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("result.jsp");
+        mv.addObject("num3", num3);
+
+        return mv;
+    }
+}
